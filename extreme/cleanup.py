dc4ccb99d921c2267a5eda3aee354a9422ce0373
Flavia Missi <flaviamissi@gmail.com>
2015-04-11 19:11:04 -0300

    create cleanup module and implement truncate based cleanup

---
+++ extreme/cleanup.py
@@ -0,0 +1,29 @@
+"""
+Testing cleanup helpers.
+These cleanup functions assume the user uses a separate settings list for local
+applications under the name of LOCAL_APPS.
+"""
+from django.db import connections
+from django.db.models import get_app, get_models
+from django.conf import settings
+
+
+def truncate_tables():
+    apps = _collect_apps()
+
+    tables = []
+    for app in apps:
+        tables.extend(_collect_model_tables(app))
+
+    # PostgreSQL only - for now
+    sql = "TRUNCATE {} RESTART IDENTITY CASCADE;".format(",".join(tables))
+    cursor = connections["default"].cursor()
+    cursor.execute(sql)
+
+
+def _collect_apps():
+    return [get_app(app_name) for app_name in settings.LOCAL_APPS]
+
+
+def _collect_model_tables(app):
+    return [model._meta.db_table for model in get_models(app)]
